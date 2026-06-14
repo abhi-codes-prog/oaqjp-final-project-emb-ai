f51aab59480532c891760333ea3122805cb48b52
abhijth <Alpha.astute@gmail.com>
2026-06-14 12:43:44 +0530

    Add URL paths for submit and show exam result

--- urls.py
+++ urls.py
@@ -1,1 +1,3 @@
-
+path('submit/', views.submit, name='submit')
+path('course/<int:course_id>/submit/', views.submit, name='submit')
+path('show_exam_result/<int:submission_id>/', views.show_exam_result, name='show_exam_result')
