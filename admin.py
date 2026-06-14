5f6f8a649d5466b5fff873082b2d9d69a5374a85
abhijth <Alpha.astute@gmail.com>
2026-06-14 12:40:17 +0530

    Add Submission model to admin registration

--- admin.py
+++ admin.py
@@ -1,21 +1,38 @@
 from django.contrib import admin
-from .models import Course, Lesson, Instructor, Learner, Enrollment, Question, Choice
+from .models import (
+    Course,
+    Lesson,
+    Instructor,
+    Learner,
+    Enrollment,
+    Question,
+    Choice,
+    Submission
+)
+
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
+    extra = 1
+
 
 class QuestionInline(admin.TabularInline):
     model = Question
+    extra = 1
+
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
 
+
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
     inlines = [QuestionInline]
+
 
 admin.site.register(Course)
 admin.site.register(Instructor)
 admin.site.register(Learner)
 admin.site.register(Enrollment)
+admin.site.register(Submission)
