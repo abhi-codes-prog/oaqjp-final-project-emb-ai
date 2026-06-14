91ad61059a60ce1a7a978028f32335999e8b870c
abhijth <Alpha.astute@gmail.com>
2026-06-14 12:52:43 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,3 +1,11 @@
-path('submit/', views.submit, name='submit')
-path('course/<int:course_id>/submit/', views.submit, name='submit')
-path('show_exam_result/<int:submission_id>/', views.show_exam_result, name='show_exam_result')
+path(
+    'course/<int:course_id>/submit/',
+    views.submit,
+    name='submit'
+),
+
+path(
+    'course/<int:course_id>/submission/<int:submission_id>/result/',
+    views.show_exam_result,
+    name='show_exam_result'
+),
