0157f28404697fc01740644460f9e0e7ac85862f
abhijth <Alpha.astute@gmail.com>
2026-06-14 12:59:59 +0530

    Fix path definitions in urls.py

--- urls.py
+++ urls.py
@@ -2,10 +2,10 @@ path(
     'course/<int:course_id>/submit/',
     views.submit,
     name='submit'
-),
+)
 
 path(
     'course/<int:course_id>/submission/<int:submission_id>/result/',
     views.show_exam_result,
     name='show_exam_result'
-),
+)
