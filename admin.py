e484fe055a8c1aff10a23c30b66471614dd9fdff
abhijth <Alpha.astute@gmail.com>
2026-06-14 12:16:32 +0530

    Refactor admin.py to simplify model registration

--- admin.py
+++ admin.py
@@ -1,23 +1,7 @@
-from django.contrib import admin
-from .models import Course, Lesson, Enrollment, Question, Choice, Submission
-
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
-
-class QuestionInline(admin.TabularInline):
-    model = Question
-    extra = 2
-
-class LessonAdmin(admin.ModelAdmin):
-    inlines = [QuestionInline]
-
-class QuestionAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInline]
-
-admin.site.register(Course)
-admin.site.register(Lesson, LessonAdmin)
-admin.site.register(Enrollment)
-admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
-admin.site.register(Submission)
+Course
+Lesson
+Instructor
+Learner
+Enrollment
+Question
+Choice
