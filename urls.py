30c56271ee9f268c1a58b3e990668c7cda393559
abhijth <Alpha.astute@gmail.com>
2026-06-14 13:44:47 +0530

    Fix URL path formatting for submit and result views

--- urls.py
+++ urls.py
@@ -1,11 +1,11 @@
 path(
-    '<int:course_id>/submit/',
+    'course/<int:course_id>/submit/',
     views.submit,
     name='submit'
-),
+)
 
 path(
     'course/<int:course_id>/submission/<int:submission_id>/result/',
     views.show_exam_result,
     name='show_exam_result'
-),
+)
