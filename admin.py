4c6c4cfd91d335cb91424c9fe481119a3102637a
abhijth <Alpha.astute@gmail.com>
2026-06-14 12:35:52 +0530

    Add admin interfaces for Question and Lesson models

--- admin.py
+++ admin.py
@@ -1,7 +1,21 @@
-Course
-Lesson
-Instructor
-Learner
-Enrollment
-Question
-Choice
+from django.contrib import admin
+from .models import Course, Lesson, Instructor, Learner, Enrollment, Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+admin.site.register(Course)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+admin.site.register(Enrollment)
